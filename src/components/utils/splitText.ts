337c44a6ccbbf7554c326f304e2dcf72662fdf53
Bharat <rathorebharat937@gmail.com>
2026-03-13 04:47:51 +0530

    Fix GSAP trial issue

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/dist/ScrollSmoother";
-import { SplitText } from "gsap/dist/SplitText";
+import ScrollSmoother from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
